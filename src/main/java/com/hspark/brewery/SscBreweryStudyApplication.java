9954f3e509082f8f2a3a1ffe2908732363f115bc
sukeypark <hspark@data-alliance.com>
2020-10-19 17:56:21 +0900

    first commit

---
+++ src/main/java/com/hspark/brewery/SscBreweryStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hspark.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscBreweryStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscBreweryStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hspark/brewery/SscBreweryStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hspark.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscBreweryStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
