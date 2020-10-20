2aa084e738b46aad99166353eb890abf96d95ead
sukeypark <hspark@data-alliance.com>
2020-10-20 10:42:05 +0900

    add domain Customer

--- src/main/java/com/hspark/brewery/domain/Customer.java
+++ src/main/java/com/hspark/brewery/domain/Customer.java
@@ -1,5 +1,39 @@
 package com.hspark.brewery.domain;
 
-public class Customer {
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Customer extends BaseEntity {
+
+    @Builder
+    public Customer(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, String customerName,
+                    UUID apiKey, Set<BeerOrder> beerOrders) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.customerName = customerName;
+        this.apiKey = apiKey;
+        this.beerOrders = beerOrders;
+    }
+
+    private String customerName;
+
+    @Column(length = 36, columnDefinition = "varchar")
+    private UUID apiKey;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
+    
 }
