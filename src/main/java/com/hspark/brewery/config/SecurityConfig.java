a68d26df30ffd7455dca239676e0d681cba1976b
sukeypark <hspark@data-alliance.com>
2020-10-20 17:31:21 +0900

    add SecurityConfig, permitAll for index, login and resources

---
+++ src/main/java/com/hspark/brewery/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hspark.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authorize -> {
+				authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll();
+			})
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin().and()
+			.httpBasic();
+	}
+	
+}
