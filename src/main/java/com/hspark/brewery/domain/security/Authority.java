99d7c361c1456d6594936ee8fce2a2bc94f36eb7
sukeypark <hspark@data-alliance.com>
2020-10-21 12:12:58 +0900

    add Entity, and how to config entity with lombok

---
+++ src/main/java/com/hspark/brewery/domain/security/Authority.java
@@ -0,0 +1,31 @@
+package com.hspark.brewery.domain.security;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Authority {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@ManyToMany(mappedBy = "authorities")
+	private Set<User> users;
+}
